1f1c8f2a3e7c8e40ab7b38d40237295ae4e9a54d
jelle1710 <25070843@student.hhs.nl>
2026-03-18 11:54:49 +0100

    colin sucks

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 import javax.swing.JFrame;
 import org.json.simple.parser.ParseException;
 
+//hi
+
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
         new Scanner(System.in);
