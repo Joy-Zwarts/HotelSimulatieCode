8f4dd856cfe62d79973869c677b109d85ffd4e1a
jelle1710 <25070843@student.hhs.nl>
2026-03-25 12:57:45 +0100

    file licker is toegevoegd, hardcoded jason file moet eruit gewerkt worden

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import org.json.simple.parser.ParseException;
 
 import javax.swing.*;
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -26,7 +27,27 @@ public class Main {
         layoutFixed.plaatsKamers();
 
 
+
         JFrame frame1 = new JFrame("Hotel Layout"); // maak een nieuw scherm aan om de simulatie te tonen
+
+                // Maak de file chooser aan
+                JFileChooser fileChooser = new JFileChooser();
+
+                // Open de file chooser
+                int result = fileChooser.showOpenDialog(frame1);
+
+                // Controleer of de gebruiker een bestand heeft geselecteerd
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    System.out.println("Geselecteerd bestand: " + selectedFile.getAbsolutePath());
+                } else {
+                    System.out.println("Geen bestand geselecteerd");
+                }
+
+
+
+
+
         frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // als je het venster sluit stopt het programma
         frame1.add(layoutFixed.getHotelPanel()); // get het panel van de layout van het hotel en voeg deze toe aan het scherm
         frame1.pack(); // pas de grootte van het scherm aan gebaseerd op de grootte van wat erin zit
