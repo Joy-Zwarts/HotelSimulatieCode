4b39dc5075767e11fb1d37df6717f23bb8a3fa20
Joy-Zwarts <25042939@student.hhs.nl>
2026-03-26 11:38:10 +0100

    MVC YAY

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        // 1. File chooser
+        // file chooser
         JFileChooser fileChooser = new JFileChooser();
         int result = fileChooser.showOpenDialog(null);
 
@@ -24,26 +24,27 @@ public class Main {
         File selectedFile = fileChooser.getSelectedFile();
         System.out.println("Geselecteerd bestand: " + selectedFile.getAbsolutePath());
 
-        // 2. Parser → Model vullen
+
+        // parser → model vullen
         LayoutParser parser = new LayoutParser();
         LayoutModel model = parser.parse(selectedFile.getAbsolutePath());
 
-        // 3. View maken
+        // view maken
         LayoutView view = new LayoutView();
 
-        // 4. Event systeem
+        // event systeem
         HotelEventManager manager = new HotelEventManager(1000);
         EventPrint printEvent = new EventPrint(manager);
 
-        // 5. Controller (doet alle layout logica)
+        // controller (doet alle layout logica)
         LayoutController controller = new LayoutController(model, view);
 
-        // 6. JFrame bouwen
+        // jframe bouwen
         JFrame frame = new JFrame("Hotel Layout");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        // Layout + events toevoegen
+        // layout + events toevoegen
         frame.add(view.getHotelPanel(), BorderLayout.CENTER);
         frame.add(printEvent.getEventPanel(), BorderLayout.EAST);
 
@@ -51,7 +52,7 @@ public class Main {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
-        // 7. Event systeem starten
+        // event systeem starten
         manager.startTimer();
     }
 }
