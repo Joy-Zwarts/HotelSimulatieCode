e8932e6d338f47387dd1dc4ce68009d4a6cd4b46
jelle1710 <25070843@student.hhs.nl>
2026-06-10 12:24:09 +0200

    nieuwe tests

---
+++ Test/Controller/Layout/GridVakjeControllerTest.java
@@ -0,0 +1,15 @@
+package Controller.Layout;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GridVakjeControllerTest {
+
+
+    @BeforeEach
+    void setup(){
+
+    }
+
+}
---
+++ Test/Model/Personen/SchoonmakerModelTest.java
@@ -0,0 +1,42 @@
+package Model.Personen;
+
+import Model.Layout.Locatie;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SchoonmakerModelTest {
+
+    SchoonmakerModel schoonmakerModel;
+
+    @BeforeEach
+    void setUp() {
+        schoonmakerModel = new SchoonmakerModel(1, new Locatie(6, 9), new Locatie(6, 7), TypePersoon.SCHOONMAKER, new Locatie(9, 11));
+    }
+
+    @Test
+    void isCleaningfalseTest() {
+
+        //assign
+        schoonmakerModel.setCleaning(false);
+
+        //assert + act
+        assertFalse(schoonmakerModel.isCleaning());
+
+    }
+    @Test
+    void isCleaningNULLTest(){
+
+        //assert + act
+        assertFalse(schoonmakerModel.isCleaning());
+    }
+    @Test
+    void isCleaningtrueTest(){
+        //assign
+        schoonmakerModel.setCleaning(true);
+
+        //assert + act
+        assertTrue(schoonmakerModel.isCleaning());
+    }
+}
